c6106c24b442b1a514c3bd25e1cac210dea95d13
Josue Rivera <josuedr11@gmail.com>
2017-03-21 18:29:16 -0430

    Added jump functionality

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
-	public float speed = 10f;
+	public float speed;
+	public float forcePower;
+	public bool grounded;
 
 	// Use this for initialization
 	void Start () {
@@ -12,11 +14,36 @@ public class CharacterMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Movement();
+		Jump();
 
-		//Left and right movement
+	}
+
+	void Movement () {
 		Vector3 newPosition = transform.position;
 		newPosition.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		transform.position = newPosition;
-		
 	}
+
+	void Jump () {
+
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+
+		if (Input.GetKey(KeyCode.W) && (grounded == true))
+		{
+			if (rb.velocity.y <= 0)
+			{
+				rb.AddForce(new Vector2(0, 20), ForceMode2D.Impulse); 
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+			grounded = true;
+		}
+
+	void OnCollisionExit2D(Collision2D coll) {
+		grounded = false;
+	}
+
 }
