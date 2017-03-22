36a9d3353460bb47c48dc78d0ced3ce29da13e4e
Josue Rivera <josuedr11@gmail.com>
2017-03-22 18:23:43 -0430

    Added Enemy and added Bullet detection script

---
+++ Assets/Scripts/BulletDetection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDetection : MonoBehaviour {
+
+	private int enemyHealth = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		if (coll.gameObject.tag == "Bullet" && (enemyHealth == 1))
+		{
+			Destroy(coll.gameObject);
+			Destroy(this.gameObject);
+		}
+		if (coll.gameObject.tag == "Bullet" && (enemyHealth == 2))
+		{
+			enemyHealth -= 1;
+			Destroy(coll.gameObject);
+		}
+
+	}
+}
