36e7ef8ec7add9fa2017cc134783a69ea07946d5
Josue Rivera <josuedr11@gmail.com>
2017-03-22 22:45:43 -0430

    Added reload and firerate functionality'

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -8,36 +8,63 @@ public class Shoot : MonoBehaviour {
 	public float bulletSpeed;
 	public bool left;
 
+	public float ammo;
+	private float fireRate = 0.3f;
+	private float nextBullet = 0.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Shooting();
+		Reloading();
+	}
 
-		if (Input.GetKeyDown(KeyCode.A)){
-			left = true;
+	void Shooting () {
+		if (ammo > 0)
+		{
+
+			if (Input.GetKeyDown(KeyCode.A)){
+				left = true;
+			}
+
+			if (Input.GetKeyDown(KeyCode.D))
+			{
+				left = false;
+			}
+
+			if (Input.GetKeyDown("space") & (left == true) & (Time.time > nextBullet))
+			{
+				nextBullet = Time.time + fireRate;
+				Vector3 newPosition = transform.position;
+				newPosition.x -= 1;
+				Instantiate(bulletTwo, newPosition, transform.rotation);
+				ammo -= 1;
+			}
+
+			if (Input.GetKeyDown("space") & (left == false) & (Time.time > nextBullet))
+			{
+				nextBullet = Time.time + fireRate;
+				Vector3 newPosition = transform.position;
+				newPosition.x += 0.5f;
+				Instantiate(bullet, newPosition, transform.rotation);
+				ammo -= 1;
+			}
 		}
 
-		if (Input.GetKeyDown(KeyCode.D))
-		{
-			left = false;
-		}
+	}
 
-		if (Input.GetKeyDown("space") & (left == true))
+	void Reloading () {
+		if (Input.GetKeyDown(KeyCode.R))
 		{
-			Vector3 newPosition = transform.position;
-			newPosition.x -= 1;
-			Instantiate(bulletTwo, newPosition, transform.rotation);
-		}
-
-		if (Input.GetKeyDown("space") & (left == false))
-		{
-			Vector3 newPosition = transform.position;
-			newPosition.x += 0.5f;
-			Instantiate(bullet, newPosition, transform.rotation);
+			ammo = 12;
 		}
 	}
-	
+
+
+
+
 }
